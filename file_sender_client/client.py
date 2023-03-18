99c0f13f3a2652f29937c934e6942642e73c5173
Ilia Demintsev <idemintsev@gmail.com>
2023-03-18 18:04:03 +0300

    Shift file sender client to anothe dir

--- file_sender_client/client.py
+++ file_sender_client/client.py
@@ -14,8 +14,11 @@ from requests.auth import HTTPBasicAuth
 from requests.exceptions import BaseHTTPError, RequestException, CompatJSONDecodeError
 
 from main.settings import Config
+from main.logger import logging
+
 
 TWO_Mb = 2 * 1024 * 1024
+logger = logging.getLogger('file_sender_client')
 
 
 @frozen(slots=True)
@@ -32,19 +35,53 @@ class FileSenderClient:
                     break
                 yield data
 
-    def send_file(self):
+    def upload_file(self):
         try:
             res = requests.post(f'{self.url}/files', data=self._read_by_chunks(), auth=HTTPBasicAuth('user', 'password'))
-            print(res.status_code, res.json())
+            logger.debug(f'{res.status_code}, {res.json()}')
         except (BaseHTTPError, RequestException, CompatJSONDecodeError) as exc:
-            print(exc)
+            logger.debug(exc)
+
+    def download_file(self):
+        try:
+            with requests.get(
+                    f'{self.url}/files/9a2c9f487988455f6bd918b1154b17225d6054cf43d404c199d1f5beb7f8a192',
+                    auth=HTTPBasicAuth('user', 'password'),
+                    stream=True
+            ) as r:
+                r.raise_for_status()
+                path_to_file = Path.joinpath(Path(__file__).parent, 'downloaded_from_server.jpg')
+                with open(path_to_file, 'wb') as f:
+                    for chunk in r.iter_content(chunk_size=TWO_Mb):
+                        f.write(chunk)
+        except (BaseHTTPError, RequestException, CompatJSONDecodeError) as exc:
+            logger.exception(f'{exc.__class__.__name__}. {str(exc)}')
+
+    def delete_file(self):
+        try:
+            res = requests.delete(
+                f'{self.url}/files/9a2c9f487988455f6bd918b1154b17225d6054cf43d404c199d1f5beb7f8a192',
+                auth=HTTPBasicAuth('user', 'password'),
+            )
+            logger.debug(f'{res.status_code}, {res.json()}')
+        except (BaseHTTPError, RequestException, CompatJSONDecodeError) as exc:
+            logger.exception(f'{exc.__class__.__name__}. {str(exc)}')
 
 
-def run():
+
+def upload_file():
     path_to_file = Path.joinpath(Path(__file__).parent, 'test_data.jpg')
     client = FileSenderClient(path_to_file)
-    client.send_file()
+    client.upload_file()
 
 
-if __name__ == '__main__':
-    run()
+def download_file():
+    path_to_file = Path.joinpath(Path(__file__).parent, 'test_data.jpg')
+    client = FileSenderClient(path_to_file)
+    client.download_file()
+
+
+def delete_file():
+    path_to_file = Path.joinpath(Path(__file__).parent, 'test_data.jpg')
+    client = FileSenderClient(path_to_file)
+    client.delete_file()
