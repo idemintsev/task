a6708addb42505cdf275a9fca0e786aa2356dd9f
Ilia Demintsev <idemintsev@gmail.com>
2023-03-18 13:34:23 +0300

    Add simple client for testing loading view

---
+++ main/file_sender_client/__init__.py
@@ -0,0 +1,1 @@
+"""Package with simple file sender client via http for testing chunk loading in Flask's app view."""
---
+++ main/file_sender_client/client.py
@@ -0,0 +1,46 @@
+"""
+Simple client for send big file by chunks.
+Use for manual testing Flask's app view FilesLoaderView.
+
+For starting client you can apply command 'poetry run send_file' from project root directory.
+For PyCharm also available use 'Run' button.
+"""
+
+import requests
+from pathlib import Path
+from attrs import frozen, field
+from typing import Generator
+from requests.auth import HTTPBasicAuth
+
+from main.settings import Config
+
+TWO_Mb = 2 * 1024 * 1024
+
+
+@frozen(slots=True)
+class FileSenderClient:
+    path_to_file: Path = field(default=None)
+    url: str = Config.FILE_SENDER_CLIENT.file_sender_client_socket
+
+    def _read_by_chunks(self, chunk_size=TWO_Mb) -> Generator:
+        """Generator to read a file piece by piece."""
+        with open(self.path_to_file, 'rb') as f:
+            while True:
+                data = f.read(chunk_size)
+                if not data:
+                    break
+                yield data
+
+    def send_file(self):
+        res = requests.post(f'{self.url}/files', data=self._read_by_chunks(), auth=HTTPBasicAuth('user', 'password'))
+        print(res.status_code, res.json())
+
+
+def run():
+    path_to_file = Path.joinpath(Path(__file__).parent, 'test_data.jpg')
+    client = FileSenderClient(path_to_file)
+    client.send_file()
+
+
+if __name__ == '__main__':
+    run()
